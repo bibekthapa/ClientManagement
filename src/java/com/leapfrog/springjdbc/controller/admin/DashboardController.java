a2bb7f9074f995e5059c27fe8f72abe4033d5fab
Bibek <bibek.thapahere@gmail.com>
2016-12-04 01:22:49 -0800

    Dashboard Controller added

---
+++ src/java/com/leapfrog/springjdbc/controller/admin/DashboardController.java
@@ -0,0 +1,18 @@
+
+package com.leapfrog.springjdbc.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/admin")
+
+public class DashboardController {
+   @RequestMapping(method = RequestMethod.GET)
+   public String index()
+   {
+       return "/admin/index";
+   }
+    
+}
---
+++ web/WEB-INF/jsp/admin/index.jsp
@@ -0,0 +1,8 @@
+<%@include file="header.jsp" %>
+
+<h1>Welcome to the Dashboard controller !! ADMIN </h1>
+
+
+
+
+<%@include file="footer.jsp" %>
